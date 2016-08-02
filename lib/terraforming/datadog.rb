0398651133faa1cb4b0cdb8fa5e3919d6a4431c2
Daisuke Fujita <dtanshi45@gmail.com>
2016-08-02 14:15:41 +0900

    bundle gem terraforming-datadog -b -t rspec

---
+++ lib/terraforming/datadog.rb
@@ -0,0 +1,7 @@
+require "terraforming/datadog/version"
+
+module Terraforming
+  module Datadog
+    # Your code goes here...
+  end
+end
---
+++ lib/terraforming/datadog/version.rb
@@ -0,0 +1,5 @@
+module Terraforming
+  module Datadog
+    VERSION = "0.1.0"
+  end
+end
